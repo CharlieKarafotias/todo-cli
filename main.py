2b8de4a65c41ea0d64a75514739e1c1284c384ad
Charles Karafotias <cnkara2023@gmail.com>
2023-08-19 10:14:04 -0400

    added subparser templates

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import argparse
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def main():
+    parser = argparse.ArgumentParser(
+        prog='Todo CLI',
+        description='A CLI for handling daily todo tasks and storing them in a YAML format',
+        epilog='Developed in 2023 by Charlie Karafotias'
+    )
+    subparsers = parser.add_subparsers(
+        title='Possible commands',
+        dest='command')
+
+    # Init parser instantiation
+    parser_init = subparsers.add_parser('init', help='Initialize TODO CLI')
+
+    # add new event parser instantiation
+    parser_add = subparsers.add_parser('add', help='Add a new todo')
+
+    args = parser.parse_args()
+    print(args)
+
+    match args.command:
+        case 'init':
+            pass
+        case 'add':
+            pass
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
---
+++ utils/init.py
@@ -0,0 +1,3 @@
+"""
+Functions needed to initialize the YAML file when the CLI is first ran
+"""
